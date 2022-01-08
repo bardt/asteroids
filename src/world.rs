9049057fd244d2fbe3e7c9334928f6be26bdf172
Roman Salnikov <roman@bardt.me>
2022-01-08 20:39:45 +0100

    Asteroids drifting

--- src/world.rs
+++ src/world.rs
@@ -18,7 +18,7 @@ impl Entity {
                 position: position.into(),
                 rotation: cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), Deg(0.0)),
             },
-            components: vec![],
+            components: vec![Component::Physics(random_physics(1., 100.))],
         }
     }
 
@@ -29,15 +29,16 @@ impl Entity {
                 position: position.into(),
                 rotation: cgmath::Quaternion::from_angle_z(Deg(rotation_angle)),
             },
-            components: vec![Component::Controllable],
+            components: vec![Component::Control],
         }
     }
 
     pub fn update(&mut self, input: &Input, dtime: &Duration) -> &Self {
         for component in &self.components {
             match component {
-                Component::Controllable => {
-                    Entity::update_controllable(&mut self.instance, input, dtime)
+                Component::Control => Entity::update_control(&mut self.instance, input, dtime),
+                Component::Physics(props) => {
+                    Entity::update_physics(&mut self.instance, props, dtime)
                 }
             }
         }
@@ -45,7 +46,7 @@ impl Entity {
         self
     }
 
-    fn update_controllable(instance: &mut Instance, input: &Input, dtime: &Duration) {
+    fn update_control(instance: &mut Instance, input: &Input, dtime: &Duration) {
         // @TODO: Base in physics
         let rotation_speed = 180.;
         let movement_speed = 50.;
@@ -53,16 +54,6 @@ impl Entity {
         let delta_time = (dtime.as_millis() as f32) / 1000.0;
         let delta_angle = delta_time * rotation_speed;
         let delta_position = delta_time * movement_speed;
-
-        if input.is_right_pressed {
-            instance.rotation =
-                instance.rotation * cgmath::Quaternion::from_angle_z(cgmath::Deg(-delta_angle))
-        }
-
-        if input.is_left_pressed {
-            instance.rotation =
-                instance.rotation * cgmath::Quaternion::from_angle_z(cgmath::Deg(delta_angle))
-        }
 
         let direction = instance.rotation.rotate_vector(cgmath::Vector3::unit_y()); //cgmath::Vector3 { x, y, z };
 
@@ -73,11 +64,62 @@ impl Entity {
         if input.is_backward_pressed {
             instance.position = instance.position - (direction * delta_position);
         }
+
+        if input.is_right_pressed {
+            instance.rotation =
+                instance.rotation * cgmath::Quaternion::from_angle_z(cgmath::Deg(-delta_angle))
+        }
+
+        if input.is_left_pressed {
+            instance.rotation =
+                instance.rotation * cgmath::Quaternion::from_angle_z(cgmath::Deg(delta_angle))
+        }
+    }
+
+    fn update_physics(instance: &mut Instance, physics_props: &PhysicsProps, dtime: &Duration) {
+        instance.position =
+            instance.position + physics_props.linear_speed * (dtime.as_millis() as f32) / 1000.0;
+        instance.rotation = cgmath::Quaternion::nlerp(
+            instance.rotation,
+            instance.rotation * physics_props.angular_speed,
+            (dtime.as_millis() as f32) / 1000.0,
+        );
     }
 }
 
 pub enum Component {
-    Controllable,
+    Control,
+    Physics(PhysicsProps),
+}
+
+pub struct PhysicsProps {
+    linear_speed: cgmath::Vector3<f32>,
+    angular_speed: cgmath::Quaternion<f32>,
+}
+
+use rand::Rng;
+
+fn random_physics(max_linear_speed: f32, max_angular_speed: f32) -> PhysicsProps {
+    let mut rng = rand::thread_rng();
+
+    let linear_speed = cgmath::Vector3 {
+        x: rng.gen_range(-max_linear_speed..max_linear_speed),
+        y: rng.gen_range(-max_linear_speed..max_linear_speed),
+        z: 0.0,
+    };
+
+    let axis = cgmath::Vector3 {
+        x: rng.gen_range(0.0..1.0),
+        y: rng.gen_range(0.0..1.0),
+        z: rng.gen_range(0.0..1.0),
+    };
+    let angle = Deg(rng.gen_range(0.0..max_angular_speed));
+    let angular_speed = cgmath::Quaternion::from_axis_angle(axis, angle);
+
+    PhysicsProps {
+        linear_speed,
+        angular_speed,
+    }
 }
 
 pub struct World {
@@ -95,7 +137,7 @@ impl World {
         ];
 
         let camera = Camera {
-            eye: (0.0, -0.01, 100.0).into(),
+            eye: (0.0, -0.01, 50.0).into(),
             // have it look at the origin
             target: (0.0, 0.0, 0.0).into(),
             // which way is "up"
